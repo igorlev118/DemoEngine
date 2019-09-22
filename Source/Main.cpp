b3e359991fa3ca6fa9b33c08d3435ee48aac83d1
Igor <igorlevdansky91@gmail.com>
2019-09-23 00:25:35 +0300

    Implement common utilities

---
+++ Source/Main.cpp
@@ -0,0 +1,10 @@
+#include "Precompiled.hpp"
+#include "Common/Build.hpp"
+
+int main()
+{
+    Debug::Initialize();
+    Build::Initialize();
+
+    return 0;
+}
---
+++ Source/Precompiled.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "Common/Debug.hpp"
+#include "Common/NonCopyable.hpp"
+#include "Common/ScopeGuard.hpp"
+#include "Common/Utility.hpp"
