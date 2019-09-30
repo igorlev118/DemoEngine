f2f291c37ae4ea1791470f3875ce7ff61fc26779
Igor Levdansky <lev-118@gmail.com>
2019-09-30 14:37:20 +0300

    Generate a source file with information about the build version

--- Build/CMake-VS2017.bat
+++ Build/CMake-VS2017.bat
@@ -7,6 +7,11 @@ cd "CMake-VS2017"
 rem Generate solution files.
 cmake -G "Visual Studio 15" ./../..
 
+if %ERRORLEVEL% NEQ 0 (
+    pause
+    exit
+)
+
 rem Open the solution file.
 echo -- Opening the solution file..
-start "" "Game Engine.sln"
+start "" "GameEngine.sln"
