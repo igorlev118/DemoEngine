079aea19b88ffd0eb594c932ae307b50cd2f9fe9
Igor Levdansky <lev-118@gmail.com>
2019-10-08 12:31:44 +0300

    Move headers from Precompiled.hpp to Engine.hpp file

--- Source/Precompiled.hpp
+++ Source/Precompiled.hpp
@@ -1,22 +1,3 @@
 #pragma once
 
-#include <cctype>
-#include <ctime>
-#include <string>
-#include <vector>
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-
-#define WIN32_LEAD_AND_MEAN
-#include <windows.h>
-
-#include <glm/glm.hpp>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-#include "Common/Debug.hpp"
-#include "Common/NonCopyable.hpp"
-#include "Common/ScopeGuard.hpp"
-#include "Common/Utility.hpp"
-#include "Logger/Logger.hpp"
+#include "Engine.hpp"
