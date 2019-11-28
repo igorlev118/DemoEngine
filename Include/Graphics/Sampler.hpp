b90b312000053d431c4c4f476e4f8935bc1c91f7
Igor Levdansky <lev-118@gmail.com>
2019-11-28 12:23:58 +0300

    Retrieve default sampler parameters from OpenGL

--- Include/Graphics/Sampler.hpp
+++ Include/Graphics/Sampler.hpp
@@ -59,6 +59,9 @@ namespace Graphics
         Sampler(RenderContext* renderContext);
         ~Sampler();
 
+        // Initializes default parameters.
+        static void InitializeDefaults();
+
         // Initializes the sampler object.
         bool Create(const SamplerInfo& info = SamplerInfo());
 
