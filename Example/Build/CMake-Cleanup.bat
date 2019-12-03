ea903373ba184785ffc6bd3a5b5bdaae779e7cc2
Igor Levdansky <lev-118@gmail.com>
2019-12-03 14:23:29 +0300

    Make example a separate project

---
+++ Example/Build/CMake-Cleanup.bat
@@ -0,0 +1,15 @@
+@echo off
+
+rem Remove temporary directories.
+:delete_folders
+for /d %%x in ("CMake-*") do rd /s /q "%%x"
+
+rem Retry if some directories cannot be deleted.
+for /d %%x in ("CMake-*") do (
+    echo Could not delete some files. Making another attempt in 4 seconds...
+    timeout /t 4 >nul
+    goto delete_folders
+)
+
+rem Remove temporary files.
+for %%x in ("*.tmp") do rm /s /q "%%x"
