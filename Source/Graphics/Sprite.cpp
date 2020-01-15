b36388d0b4b82bf3d1f6fe6724c64f14e07509ef
Igor <igorlevdansky91@gmail.com>
2020-01-15 12:49:13 +0300

    Commit an initial implementation of sprite renderer that is still not quite working yet

---
+++ Source/Graphics/Sprite.cpp
@@ -0,0 +1,27 @@
+#include "Precompiled.hpp"
+#include "Graphics/Sprite.hpp"
+using namespace Graphics;
+
+Sprite::Info::Info() :
+    texture(nullptr),
+    transparent(false),
+    filter(true)
+{
+}
+
+bool Sprite::Info::operator==(const Info& other) const
+{
+    return texture == other.texture && transparent == other.transparent && filter == other.filter;
+}
+
+bool Sprite::Info::operator!=(const Info& other) const
+{
+    return texture != other.texture || transparent != other.transparent || filter != other.filter;
+}
+
+Sprite::Data::Data() :
+    transform(1.0f),
+    rectangle(0.0f, 0.0f, 1.0f, 1.0f),
+    color(1.0f, 1.0f, 1.0f, 1.0f)
+{
+}
